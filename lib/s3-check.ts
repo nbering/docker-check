c77d2cfce7c5c3dbb054ad34b5a3e090de173634
Nicholas Bering <bering.nicholas@gmail.com>
2017-12-16 14:44:06 -0500

    Test for s3:GetObjectVersion

--- lib/s3-check.ts
+++ lib/s3-check.ts
@@ -6,7 +6,9 @@ import {S3_BUCKET, S3_PREFIX} from "./config";
 
 export const s3Router = express.Router();
 
-s3Router.get('/s3-check', testSetup, testPutObject, testGetObject, testListObjects, doneTests)
+s3Router.get('/s3-check', testSetup, testPutObject, testGetObject,
+    testDeleteObject, testGetObjectVersion, testListObjectVersions,
+    testListObjects, doneTests)
 
 function testSetup(
     req: IS3Request,
@@ -49,7 +51,9 @@ function testPutObject(
             success: !err
         };
         test.results.push(results);
-        next(err);
+        if (result && result.VersionId)
+            test.savedObjectVersion = result.VersionId;
+        next();
     });
 }
 
@@ -76,7 +80,98 @@ function testGetObject(
             success: !err
         };
         test.results.push(results);
-        next(err);
+        next();
+    });
+}
+
+function testDeleteObject(
+    req: IS3Request,
+    res: express.Response,
+    next: express.NextFunction
+)
+{
+    if (!req.test)
+        return next(new Error("Test properties missing on request object."));
+    let test = req.test;
+    if (checkForError(test.results))
+        return next();
+    
+    let params = {
+        Bucket: req.test.bucket,
+        Key: `${S3_PREFIX}${req.test.guid}`
+    }
+    
+    req.test.client.deleteObject(params, (err, result) => {
+        let results: IS3TestResult = {
+            action: "s3:DeleteObject",
+            success: !err
+        };
+        test.results.push(results);
+        next();
+    });
+}
+
+function testListObjectVersions(
+    req: IS3Request,
+    res: express.Response,
+    next: express.NextFunction
+)
+{
+    if (!req.test)
+        return next(new Error("Test properties missing on request object."));
+    let test = req.test;
+    if (checkForError(test.results))
+        return next();
+    
+    let params = {
+        Bucket: req.test.bucket,
+        Prefix: `${S3_PREFIX}${req.test.guid}`
+    }
+    
+    req.test.client.listObjectVersions(params, (err, result) => {
+        let results: IS3TestResult = {
+            action: "s3:ListBucketVersions",
+            success: !err
+        };
+        test.results.push(results);
+        next();
+    });
+}
+
+function testGetObjectVersion(
+    req: IS3Request,
+    res: express.Response,
+    next: express.NextFunction
+)
+{
+    if (!req.test)
+        return next(new Error("Test properties missing on request object."));
+    let test = req.test;
+    if (checkForError(test.results))
+        return next();
+    if (!test.savedObjectVersion){
+        test.results.push({
+            action:"s3:GetObjectVersion",
+            success: false,
+            message: "Did not have a vesion ID to get. Is versioning enabled?"
+        });
+        return next()
+    }
+    
+    let params = {
+        Bucket: req.test.bucket,
+        Key: `${S3_PREFIX}${req.test.guid}`,
+        VersionId: test.savedObjectVersion
+    }
+    
+    req.test.client.getObject(params, (err, result) => {
+        let results: IS3TestResult = {
+            action: "s3:GetObjectVersion",
+            success: !err,
+            message: `Object version: ${test.savedObjectVersion}`
+        };
+        test.results.push(results);
+        next();
     });
 }
 
@@ -100,7 +195,7 @@ function testListObjects(
             success: !err
         };
         test.results.push(results);
-        next(err);
+        next();
     });
 }
 
@@ -133,6 +228,7 @@ interface IS3Request extends express.Request{
         guid: string;
         client: S3;
         bucket: string;
+        savedObjectVersion?: string;
     }
 }
 
