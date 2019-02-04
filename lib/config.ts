60f0ecad0bc853c2d3d4cfb805e5e55e668ef94f
Nicholas Bering <bering.nicholas@gmail.com>
2019-02-04 16:15:49 -0500

    More flexible config loading as JSON from environment sources.

--- lib/config.ts
+++ lib/config.ts
@@ -8,12 +8,16 @@ export const S3_PREFIX = process.env["S3_PREFIX"] || "tests/";
 var configJSON: string;
 
 if (typeof process.env["DOCKER_CHECK_CONFIG"] === "string") {
+    console.log("Collecting CONFIG from DOCKER_CHECK_CONFIG.")
     configJSON = <string>process.env["DOCKER_CHECK_CONFIG"];
 } else if (typeof process.env["DOCKER_CHECK_CONFIG_B64"] === "string") {
+    console.log("Collecting CONFIG from DOCKER_CHECK_CONFIG_B64.")
     configJSON = Buffer.from(<string>process.env["DOCKER_CHECK_CONFIG_B64"], "base64").toString("utf8");
 } else if (typeof process.env["DOCKER_CHECK_CONFIG_FILE"] === "string") {
+    console.log("Collecting CONFIG from DOCKER_CHECK_CONFIG_FILE.")
     configJSON = readFileSync(<string>process.env["DOCKER_CHECK_CONFIG_FILE"], "utf8")
 } else {
+    console.log("No config found in environment, defaulting to empty config object.")
     configJSON = "{}"
 }
 
@@ -23,6 +27,7 @@ function tryParseJSON(str: string): any{
     try {
         return JSON.parse(str)
     } catch (e) {
+        console.error("Failed to parse JSON configuration...")
         return;
     }
 }
